d84cad94e818f41385aff20199e2b855d07892fd
Marco Daniel <marco11.mendia@gmail.com>
2023-01-03 23:13:04 -0600

    Se agrega router, links al menú y la configuración inicial de telwindcss

---
+++ src/components/molecules/header/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <div>
+            <img src="https://app.ed.team/images/logo/isotipo-color.svg" alt="Logo eCommerce" />
+        </div>
+    )
+}
+
+export default Logo
